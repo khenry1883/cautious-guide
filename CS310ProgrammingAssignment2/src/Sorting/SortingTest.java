c76c07fd11edfd47715a0a4308cfe0d01ea98e97
Kareena Henry <supergirl.henry@gmail.com>
2020-04-16 07:12:37 -0700

    Initial commit.

---
+++ CS310ProgrammingAssignment2/src/Sorting/SortingTest.java
@@ -0,0 +1,53 @@
+package Sorting;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class SortingTest<E> implements Sorting<E> {
+
+	// constructor
+	public SortingTest() {
+
+	}
+
+	public static void main(String[] args) {
+		String str = "";
+		int n = 10;
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		SortingTest sort = new SortingTest();
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+			list.add(random.nextInt());
+		}
+		System.out.print(str += list);
+		sort.insertionSort(list, 0, 0, false);
+		System.out.print("end");
+
+	}
+
+	@Override
+	public void insertionSort(LinkedList<E> list, int lowindex, int highindex, boolean reversed) {
+		for (int nextPos = 1; nextPos < list.size(); nextPos++) {
+			Comparable nextVal = (Comparable) list.get(nextPos);
+			while (nextPos > 0 && nextVal.compareTo(list.get(nextPos - 1)) < 0) {
+				list.remove(nextPos);
+				list.add(nextPos - 1, (E) nextVal);
+			}
+			list.add(nextPos, (E) nextVal);
+		}
+		System.out.println("" + list);
+	}
+
+	@Override
+	public void quicksort(LinkedList<E> list, int lowindex, int highindex, boolean reversed) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mergeSortLL(LinkedList<E> list, boolean reversed) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
